c4dc099db1633602ef0553f288b104f9d226f8b4
MayFerrer <maymariejuanferrer@example.com>
2025-03-12 16:00:39 +0800

    Uploading project files

---
+++ May_midtermproject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="May_midtermproject.Global" Language="C#" %>
---
+++ May_midtermproject/WeddingRegistration.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.UI;
+
+namespace May_midtermproject
+{
+    public partial class WeddingRegistration : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            // After clicking register, show the confirmation modal using JavaScript
+            ClientScript.RegisterStartupScript(this.GetType(), "Popup", "showConfirmationModal();", true);
+        }
+    }
+}
